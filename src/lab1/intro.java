0faf6dd85ff063847330d5e8f939ae2938a20f71
thanyathon <thanyathon2418@gmail.com>
2018-11-25 16:05:14 +0700

    test upload to Github

---
+++ src/lab1/intro.java
@@ -0,0 +1,21 @@
+package lab1;
+
+public class intro {
+
+
+    public static void main(String[] args) {
+        System.out.println("thanyathon kulcharoen");
+
+
+
+
+
+
+    }//main
+
+
+
+}//class
+
+
+
